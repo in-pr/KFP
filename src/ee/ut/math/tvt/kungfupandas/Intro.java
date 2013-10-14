6885c88a4fa9c8e485c4195ce9299566ee78abcb
ivari <ivari.tolp@ut.ee>
2013-10-14 23:13:27 +0300

    added required .java files

---
+++ src/ee/ut/math/tvt/kungfupandas/Intro.java
@@ -0,0 +1,9 @@
+package ee.ut.math.tvt.kungfupandas;
+
+public class Intro {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+
+}
---
+++ src/ee/ut/math/tvt/kungfupandas/IntroUI.java
@@ -0,0 +1,9 @@
+package ee.ut.math.tvt.kungfupandas;
+
+public class IntroUI {
+
+	public IntroUI() {
+		// TODO Draw the UI
+	}
+
+}
