2b33c079ce0e23d0da334e8bcca29f420f72ab5f
ivari <ivari.tolp@ut.ee>
2013-10-15 18:22:20 +0300

    app now functional; additional build.xml changes

--- src/ee/ut/math/tvt/kungfupandas/Intro.java
+++ src/ee/ut/math/tvt/kungfupandas/Intro.java
@@ -1,9 +1,27 @@
 package ee.ut.math.tvt.kungfupandas;
 
+import java.io.IOException;
+import java.util.Properties;
+
 public class Intro {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		Properties application = getParams("application");
+		Properties version = getParams("version");
+		new IntroUI(application, version.getProperty("build.major.number")
+				+ "." + version.getProperty("build.minor.number") + "."
+				+ version.getProperty("build.revision.number"));
+	}
+
+	private static Properties getParams(String filename) {
+		Properties params = new Properties();
+		try {
+			params.load(Intro.class.getClassLoader().getResourceAsStream(
+					filename + ".properties"));
+		} catch (IOException e) {
+			// fails quietly and returns empty properties stack
+		}
+		return params;
 	}
 
 }
--- src/ee/ut/math/tvt/kungfupandas/IntroUI.java
+++ src/ee/ut/math/tvt/kungfupandas/IntroUI.java
@@ -1,9 +1,49 @@
 package ee.ut.math.tvt.kungfupandas;
 
-public class IntroUI {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 
-	public IntroUI() {
-		// TODO Draw the UI
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class IntroUI extends JFrame {
+
+	private static final long serialVersionUID = 1L; // class version
+
+	// constructs the user interface
+	public IntroUI(Properties params, String version) {
+		// sets window properties
+		super(params.getProperty("team.name"));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocation(100, 100);
+		setSize(500, 400);
+		setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+		
+		// makes components
+		JLabel[] bits = new JLabel[6];
+		bits[0] = new JLabel(new ImageIcon(getClass().getClassLoader()
+				.getResource(params.getProperty("team.logo"))));
+		bits[1] = new JLabel("Team Name: " + params.getProperty("team.name"));
+		bits[2] = new JLabel("Team Leader: " + params.getProperty("team.leader"));
+		bits[3] = new JLabel("Team Leader Email: " + params.getProperty("team.leader.email"));
+		bits[4] = new JLabel("Team Members: " + params.getProperty("team.members"));
+		bits[5] = new JLabel("Software Version: " + version);
+
+		// adds components
+		for (JLabel x : bits) {
+			x.setHorizontalAlignment(JLabel.CENTER);
+			add(x);
+			add(Box.createVerticalGlue());
+		}
+
+		// displays the window
+		repaint();
+		setVisible(true);
 	}
 
 }
