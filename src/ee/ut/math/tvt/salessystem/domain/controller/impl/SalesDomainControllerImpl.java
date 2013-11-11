b31c499f218715034c7279b7f985c3b11916c344
GetterKrutto <getter88@hotmail.com>
2013-11-11 23:37:05 +0200

    saving data to database (parandus)

--- src/ee/ut/math/tvt/salessystem/domain/controller/impl/SalesDomainControllerImpl.java
+++ src/ee/ut/math/tvt/salessystem/domain/controller/impl/SalesDomainControllerImpl.java
@@ -3,6 +3,8 @@ package ee.ut.math.tvt.salessystem.domain.controller.impl;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.Session;
+
 import ee.ut.math.tvt.salessystem.domain.exception.VerificationFailedException;
 import ee.ut.math.tvt.salessystem.domain.controller.SalesDomainController;
 import ee.ut.math.tvt.salessystem.domain.data.HistoryItem;
@@ -30,10 +32,15 @@ public class SalesDomainControllerImpl implements SalesDomainController {
 		// XXX - Save purchase
 		HistoryItem newHistoryItem = new HistoryItem(goods);
 		model.getHistoryTableModel().addItem(newHistoryItem);
+		Session em = HibernateUtil.currentSession();
+		em.getTransaction().begin();
 		for (SoldItem sItem : goods) {
 			sItem.getStockItem().setQuantity(
 					sItem.getStockItem().getQuantity() - sItem.getQuantity());
+			em.update(sItem.getStockItem());
 		}
+		
+		em.getTransaction().commit();
 	}
 
 	public void cancelCurrentPurchase() throws VerificationFailedException {
