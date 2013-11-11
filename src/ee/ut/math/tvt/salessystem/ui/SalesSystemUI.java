262d8f4f75ed885575a946eb5bbaa51485343aad
GetterKrutto <getter88@hotmail.com>
2013-11-11 14:43:24 +0200

    Modified the class ee.ut.math.tvt.salessystem.ui.ConsoleUI. When the "q"button is pressed, application will end the database session before the exit.

--- src/ee/ut/math/tvt/salessystem/ui/SalesSystemUI.java
+++ src/ee/ut/math/tvt/salessystem/ui/SalesSystemUI.java
@@ -1,19 +1,25 @@
 package ee.ut.math.tvt.salessystem.ui;
 
 import com.jgoodies.looks.windows.WindowsLookAndFeel;
+
 import ee.ut.math.tvt.salessystem.domain.controller.SalesDomainController;
 import ee.ut.math.tvt.salessystem.ui.model.SalesSystemModel;
 import ee.ut.math.tvt.salessystem.ui.tabs.HistoryTab;
 import ee.ut.math.tvt.salessystem.ui.tabs.PurchaseTab;
 import ee.ut.math.tvt.salessystem.ui.tabs.StockTab;
+
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+
 import javax.swing.JFrame;
 import javax.swing.JTabbedPane;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+
 import org.apache.log4j.Logger;
 
 /**
@@ -69,6 +75,9 @@ public class SalesSystemUI extends JFrame {
 		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
 		setLocation((screen.width - width) / 2, (screen.height - height) / 2);
 
+		
+		
+		
 		addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent e) {
