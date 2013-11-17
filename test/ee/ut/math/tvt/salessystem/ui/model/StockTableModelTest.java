5934050c7c06227b35a180368ce511cd67d245aa
GetterKrutto <getter88@hotmail.com>
2013-11-17 18:08:07 +0200

    stockTableModelTest3

--- test/ee/ut/math/tvt/salessystem/ui/model/StockTableModelTest.java
+++ test/ee/ut/math/tvt/salessystem/ui/model/StockTableModelTest.java
@@ -1,5 +1,19 @@
 package ee.ut.math.tvt.salessystem.ui.model;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class StockTableModelTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
 
 }
