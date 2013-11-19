0df09c53c486a3735d0e770e7f20587cd0082b57
in-pr <in-pr@hotmail.com>
2013-11-19 13:27:24 +0200

    modified StockItemTest

--- task1/test/ee/ut/math/tvt/salessystem/domain/data/StockItemTest.java
+++ task1/test/ee/ut/math/tvt/salessystem/domain/data/StockItemTest.java
@@ -11,7 +11,7 @@ public class StockItemTest {
 
 	@Before
 	public void setUp() {
-		item1 = new StockItem(1L, "Lauaviin", "Joodiku parim kaaslane", 3.50);
+		item1 = new StockItem(1L, "Lauaviin", "Joodiku parim kaaslane", 3.50, 2);
 	}
 
 	@Test
@@ -20,12 +20,18 @@ public class StockItemTest {
 		// StockItem item3 = new StockItem(1L, "Lauaviin",
 		// "Joodiku parim kaaslane", 3.50);
 
-		assertEquals(item1, item2);
+		assertEquals(item1.getId(), item2.getId());
+		assertEquals(item1.getName(), item2.getName());
+		assertEquals(item1.getDescription(), item2.getDescription());
+		assertEquals(item1.getPrice(), item2.getPrice(), 0.0001);
+		assertEquals(item1.getQuantity(), item2.getQuantity());
 	}
 
 	@Test
 	public void testGetColumn() {
+		assertEquals(1L, item1.getColumn(0));
+		assertEquals("Lauaviin", item1.getColumn(1));
 		assertEquals(3.50, item1.getColumn(2));
-
+		assertEquals(2, item1.getColumn(3));
 	}
 }
